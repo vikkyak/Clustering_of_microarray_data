de40307177942c08d5f4c1bc43cc5ac8a424f8bf
vIkAsSiNgH <76863611+vikkyak@users.noreply.github.com>
2024-03-26 12:41:46 +0900

    Delete accuracy.m

--- accuracy.m
+++
@@ -1,8 +0,0 @@
-
-pre_label=[ones(50,1);ones(50,1).*2;ones(50,1).*3];
-idx = [index1, index2, index3]';
-idx = index';
-pre_label =label;
-[Acc,rand_index,match]=AccMeasure(pre_label,idx);
-NMI_val = nmi(pre_label, idx);
-NMI_val = NMI_val*100;
